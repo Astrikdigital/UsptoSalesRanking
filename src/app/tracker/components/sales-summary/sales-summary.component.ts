7b050ae18b9c77d9ca26908a2ba68dbf893e4c25
IRFAN ALI <145743331+DevNabeelKhan@users.noreply.github.com>
2025-01-22 00:48:18 +0500

    confetti effect on agents sale add socket

--- src/app/tracker/components/sales-summary/sales-summary.component.ts
+++ src/app/tracker/components/sales-summary/sales-summary.component.ts
@@ -6,6 +6,8 @@ import { HttpService } from '../../../shared/services/http.service';
 import { AchievementModalComponent } from '../../modals/achievement-modal/achievement-modal.component';
 import { RankingService } from '../../../shared/services/rankingService';
 import { HttpApiService } from '../../../shared/services/http-api-service';
+import confetti from 'canvas-confetti';
+
 
 @Component({
   selector: 'app-sales-summary',
@@ -17,7 +19,7 @@ export class SalesSummaryComponent {
 
   rankings: any = [];
   teamsData: any = [];
-  agentOfDay:any;
+  agentOfDay: any;
   currentDate: any = new Date();
 
 
@@ -33,25 +35,22 @@ export class SalesSummaryComponent {
   }
 
   ngOnInit() {
-    this.getAgentoftheDay();
-this.getTopTeams();
-this.getPerformers();
-    // this.myService.getTeams()
-    //   .subscribe(response => {
-    //     this.teamsData = response;
-
-    //   });
     this.rankingService.startConnection();
-    this.rankingService.onRankingUpdate((data) => { 
+    this.getAgentoftheDay();
+    this.getTopTeams();
+    this.getPerformers();
+
+    this.rankingService.onAddSaleAgent((data) => {
       this.rankings = data.list;
+      this.celebrate();
       this.getTopTeams();
-      this.OpenModal(data,0);
+      this.OpenModal(data, 0);
     });
 
-    this.rankingService.onRefundSale((data) => { 
+    this.rankingService.onRefundSale((data) => {
       this.rankings = data.list;
       this.getTopTeams();
-      this.OpenModal(data,1);
+      this.OpenModal(data, 1);
     });
 
     this.rankingService.onTeamRankingUpdate((data) => {
@@ -76,22 +75,35 @@ this.getPerformers();
   async getAgentoftheDay() {
     let res: any = await this.httpService.getAgentoftheDay();
     this.agentOfDay = res[0]
-    console.log(this.agentOfDay,'agent of the day');
-    
+    console.log(this.agentOfDay, 'agent of the day');
+
     // this.rankings.sort((a:any, b:any) => a.Rank - b.Rank)
   }
 
-  OpenModal(data:any,isRefund:number) {
+  OpenModal(data: any, isRefund: number) {
     debugger
     data.isRefund = isRefund
     this.dialog.open(AchievementModalComponent, {
       data: data,
-      panelClass:'insert-refund',
+      panelClass: 'insert-refund',
       height: '600px',
       width: '1000px',
     })
     // setTimeout(() => {
     //   this.dialog.closeAll();
     // }, 5000);
+  }
+
+  celebrate() {
+    const duration = 3000; // in milliseconds
+
+    confetti({
+      particleCount: 150,
+      spread: 180,
+      origin: { y: 0.6 },
+      colors: ['#FF4500', '#008080', '#FFD700'],
+    });
+    // Clear confetti after a certain duration
+    setTimeout(() => confetti.reset(), duration);
   }
 }
